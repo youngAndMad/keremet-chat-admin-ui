2893f5d9d799b2d71dbb6d3fb9bd063af90586d2
youngandmad <kkraken2005@gmail.com>
2024-05-30 21:05:04 +0600

    extended login form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -6,6 +6,7 @@ import { FaLock } from "react-icons/fa6";
 import "./Login.scss";
 import { useMutation } from "@tanstack/react-query";
 import api from "@/libs/api";
+import CommonBackground from "@/public/common-background.svg";
 
 const allowedRoles = [
   SecurityRoleType.ROLE_APPLICATION_MANAGER,
@@ -45,19 +46,12 @@ export default function Page() {
     <div className="container">
       <div className="session">
         <div className="left">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 300 302.5"
-            enableBackground="new 0 0 300 302.5"
-            version="1.1"
-            xmlSpace="preserve"
-          >
-            <style>{`.st01{fill:#fff;}`}</style>
-            <path
-              className="st01"
-              d="m126 302.2c-2.3 0.7-5.7 0.2-7.7-1.2l-105-71.6c-2-1.3-3.7-4.4-3.9-6.7l-9.4-126.7c-0.2-2.4 1.1-5.6 2.8-7.2l93.2-86.4c1.7-1.6 5.1-2.6 7.4-2.3l125.6 18.9c2.3 0.4 5.2 2.3 6.4 4.4l63.5 110.1c1.2 2 1.4 5.5 0.6 7.7l-46.4 118.3c-0.9 2.2-3.4 4.6-5.7 5.3l-121.4 37.4zm63.4-102.7c2.3-0.7 4.8-3.1 5.7-5.3l19.9-50.8c0.9-2.2 0.6-5.7-0.6-7.7l-27.3-47.3c-1.2-2-4.1-4-6.4-4.4l-53.9-8c-2.3-0.4-5.7 0.7-7.4 2.3l-40 37.1c-1.7 1.6-3 4.9-2.8 7.2l4.1 54.4c0.2 2.4 1.9 5.4 3.9 6.7l45.1 30.8c2 1.3 5.4 1.9 7.7 1.2l52-16.2z"
-            />
-          </svg>
+          <img
+            src={CommonBackground.src}
+            alt={"Keremet Chat"}
+            width="64"
+            height="64"
+          />
         </div>
         <form className="log-in" onSubmit={(e) => handleSubmit(e)}>
           <h4>
@@ -95,7 +89,19 @@ export default function Page() {
             <div className="icon">
               <FaLock />
             </div>
-            <button type="submit">Log in</button>
+          </div>
+          <button type="submit">Log in</button>
+          <div className="links">
+            <div className="link-wrapper">
+              <a
+                className="link"
+                href="https://github.com/youngAndMad/keremet-chat?tab=readme-ov-file"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn about Keremet chat
+              </a>
+            </div>
           </div>
         </form>
       </div>
